02471242c9233ac1543d2909895c2dd21128a294
Clayon Stewart <c.stewart89@gmail.com>
2021-03-05 21:56:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,9 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  puts badge_maker(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+    
+  assign_rooms(attendees)
 end
